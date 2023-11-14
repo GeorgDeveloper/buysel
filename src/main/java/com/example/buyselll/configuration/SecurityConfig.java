a0d293816796552686a7c82efd04b6c49d64b911
egor <ifreyd@yandex.ru>
2023-11-14 12:40:11 +0300

    Lesson_5_2

--- src/main/java/com/example/buyselll/configuration/SecurityConfig.java
+++ src/main/java/com/example/buyselll/configuration/SecurityConfig.java
@@ -1,4 +1,18 @@
 package com.example.buyselll.configuration;
 
-public class SecurityConfig {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        super.configure(http);
+    }
+
+
 }
