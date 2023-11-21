77cf5dbb373cb917ec933056196783fd1bfaacc5
Georgvit <ifreyd@mail.ru>
2023-11-21 22:47:13 +0300

    lesson_07_1

--- src/main/java/com/example/buyselll/models/User.java
+++ src/main/java/com/example/buyselll/models/User.java
@@ -49,6 +49,12 @@ public class User implements UserDetails {
     }
 
     // security
+
+
+    public boolean isAdmin() {
+        return roles.contains(Role.ROLE_ADMIN);
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles;
